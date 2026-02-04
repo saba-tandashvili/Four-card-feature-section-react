44187e03cb9eca8bfc74a6aa906d270b17cc4d91
saba-tandashvili <sb.tandashvili@gmail.com>
2026-02-04 21:32:48 +0400

    commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./App.css"
+import equi from "./images/image-equilibrium.jpg"
+import eth from "./images/icon-ethereum.svg"
+import clock from "./images/icon-clock.svg"
+import avatar from "./images/image-avatar.png"
+import view from "./images/icon-view.svg"
+
+function App() {
+	return (
+		<div className="Card">
+			<div className="banner">
+				<img src={equi} alt="" className="equi" />
+				<div>
+					<img src={view} alt="" className="view" />
+				</div>
+			</div>
+			<p className="title" >
+				Equilibrium #3429
+			</p>
+			<p className="description">
+				Our Equilibrium collection promotes balance and calm.
+			</p>
+			<div className="group-4">
+				<div className="left">
+					<img src={eth} alt="" height="18px" />
+					<p className="caption">0.041 ETH</p>
+				</div>
+				<div className="right">
+					<img src={clock} alt="" height="16px" />
+					<p className="time-left">3 days left</p>
+				</div>
+			</div>
+			<div className="line"></div>
+			<div className="author">
+				<img src={avatar} alt="" className="avatar" />
+				<span>
+					Creation of{" "}
+					<p className="author-link" >
+						Jules Wyvern
+					</p>
+				</span>
+			</div>
+		</div>
+	)
+}
+
+export default App
